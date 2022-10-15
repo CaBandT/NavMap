fd6f3572bf7e53bb1c15a5f08a12edeb81d170e5
Joel Robb <joelmrobb@gmail.com>
2022-10-15 09:35:46 +0200

    Joel

--- NavMap/app/src/main/java/com/example/navmap/MainActivity.java
+++ NavMap/app/src/main/java/com/example/navmap/MainActivity.java
@@ -24,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
             {
                 Intent intent = new Intent(MainActivity.this, NavMain.class);
                 startActivity(intent);
-                //test CBT is an activity 
+                //test CBT is an activity
+
+                //this is a change by joel
             }
         });
     }
