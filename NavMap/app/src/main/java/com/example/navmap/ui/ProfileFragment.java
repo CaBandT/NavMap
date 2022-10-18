50fbd65c7d609315656d6ddae1f231f1d66ec25c
Joel Robb <joelmrobb@gmail.com>
2022-10-18 16:52:27 +0200

    Profile Settings
    Updated the code and xml for the profile fragment.

--- NavMap/app/src/main/java/com/example/navmap/ui/ProfileFragment.java
+++ NavMap/app/src/main/java/com/example/navmap/ui/ProfileFragment.java
@@ -1,22 +1,34 @@
 package com.example.navmap.ui;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.navmap.LoginActivity;
 import com.example.navmap.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileFragment extends Fragment {
 
     private Activity activity;
-    TextView fragmentMsg;
+
+    Spinner spinner;
+    TextView Nametxt, Emailtxt;
+    Button logout;
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,8 +41,39 @@ public class ProfileFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        fragmentMsg = activity.findViewById(R.id.text_profile);
+        logout = activity.findViewById(R.id.btnLogout);
+        spinner = activity.findViewById(R.id.spinner);
 
-        fragmentMsg.setText("This is the profile fragment.");
+        activity = getActivity();
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        if (user != null) {
+            Nametxt = activity.findViewById(R.id.displayName);
+            Emailtxt = activity.findViewById(R.id.displayEmail);
+            // Name, email address, and profile photo Url
+
+            String name = auth.getCurrentUser().getDisplayName();
+            Log.d("Profile name", "name: " + name);
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+            Emailtxt.setText(email);
+            Nametxt.setText(name);
+            boolean emailVerified = user.isEmailVerified();
+
+            String uid = user.getUid();
+
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                Intent intent = new Intent(activity, LoginActivity.class);
+                startActivity(intent);
+                activity.finish();
+            }
+        });
     }
 }
